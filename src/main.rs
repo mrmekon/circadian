2a3e5cc32b65d9a4ea9fe76f8e3d0e4fe07e08a3
Trevor Bentley <trevor@trevorbentley.com>
2017-11-13 21:17:03 +0100

    Add GPL license info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2017 Trevor Bentley
+ *
+ * Author: Trevor Bentley
+ * Contact: trevor@trevorbentley.com
+ * Source: https://github.com/mrmekon/circadian
+ *
+ * This file is part of Circadian.
+ *
+ * Circadian is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Circadian is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Circadian.  If not, see <http://www.gnu.org/licenses/>.
+ */
 extern crate regex;
 use regex::Regex;
 
